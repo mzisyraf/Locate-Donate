3d11c903297a384e5af78dbebb47ce3383495332
Marvin Chin Yi Kai <u2000490@siswa.um.edu.my>
2022-01-23 11:57:39 +0800

    Added some extra features

--- server.R
+++ server.R
@@ -1,12 +1,9 @@
 library(shiny)
-
 library(dplyr)
-
 library(leaflet)
-
 library(DT)
 
-shinyServer(function(input, output) { 
+shinyServer(function(input, output, session) { 
   
   # Import Data and clean it
   
@@ -20,35 +17,42 @@ shinyServer(function(input, output) {
   # new column for the popup label
   
   lnd_data2 <- mutate(lnd_data, cntnt=paste0('<strong>Name: </strong>',Name,
-                                          '<br><strong>Address:</strong> ', Address,
-                                          '<br><strong>Contact:</strong> ',Contact,
-                                          '<br><strong>Email:</strong> ',Email,
-                                          '<br><strong>Website:</strong> ',Website))
-  
+                                             '<br><strong>Address:</strong> ', Address,
+                                             '<br><strong>Contact:</strong> ',Contact,
+                                             '<br><strong>Email:</strong> ',Email,
+                                             '<br><strong>Website:</strong> ',Website))
   
   # create a color palette for category type in the data file
   
   pal <- colorFactor(pal = c("#1b9e77", "#d95f02", "#7570b3", "#000000"), domain = c("Blood", "Clothes", "Food", "Recycle"))
   
-  # create the leaflet map  
-
+  # create the leaflet map according to user input 
+  filteredData <- reactive({
+    if (input$donations == "All donations") {
+      lnd_data2
+    } else {
+      filter(lnd_data2, Category == input$donations)
+    }
+  })
+  
   
   output$map <- renderLeaflet({
-    leaflet(lnd_data) %>% 
+    leaflet(filteredData()) %>% 
       addCircles(lng = ~Longitude, lat = ~Latitude) %>% 
       addTiles() %>%
-      addCircleMarkers(data = lnd_data2, lat =  ~Latitude, lng =~Longitude, 
-                       radius = 5, popup = ~as.character(cntnt), 
+      addCircleMarkers(lat =  ~Latitude, lng =~Longitude, 
+                       radius = 10, popup = ~as.character(cntnt), 
                        color = ~pal(Category),
-                       stroke = FALSE, fillOpacity = 0.8)%>%
+                       stroke = FALSE, fillOpacity = 0.9)%>%
       addLegend(pal=pal, values=lnd_data$Category,opacity=1, na.label = "Not Available")%>%
       addEasyButton(easyButton(
         icon="fa-crosshairs", title="ME",
         onClick=JS("function(btn, map){ map.locate({setView: true}); }")))
   })
   
+  
+  
   #create a data object to display data
-
   
   output$data <-DT::renderDataTable(datatable(
     lnd_data[,c(1,2,5,6,7:8)],filter = 'top',
--- ui.R
+++ ui.R
@@ -1,8 +1,24 @@
 library(shiny)
+library(shinyWidgets)
 library(leaflet)
+library(tidyverse)
 
 navbarPage("Locate and Donate", id="main",
-           tabPanel("Map", leafletOutput("map", height=1000)),
+           tabPanel("Map", tags$style(type = "text/css", "html, body {width:100%;height:100%}"), 
+                    leafletOutput("map", height=1000),
+                    absolutePanel(top = 85, left = 90,
+                                  pickerInput("donations", label = "Select a donation:",
+                                              choices = list("All donations", 
+                                                             "Blood",
+                                                             "Clothes",
+                                                             "Food",
+                                                             "Recycle"),
+                                              options = list(
+                                                
+                                                `live-search` = TRUE)
+                                  )
+                    ),
+                    actionButton("NewLocation", "Add more")
+           ),
            tabPanel("Data", DT::dataTableOutput("data")),
            tabPanel("Read Me",includeMarkdown("readme.md")))
-
